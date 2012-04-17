db6e07e40a5c101400f5caacabefa2b77dee198f
Romain THERRAT <romain42@gmail.com>
2012-04-17 22:23:32 +0200

    Add calculator unit test

---
+++ test/unit/CalculatorTest.php
@@ -0,0 +1,27 @@
+<?php
+  
+require_once( dirname(__FILE__)."/../bootstrap/unit.php");
+
+$t = new lime_test(13);
+
+// Test add function
+$t->is(Calculator::add(1,2),3);
+$t->is(Calculator::add(1.5,2.5),4);
+$t->is(Calculator::add(1.123,2),3.123);
+
+// Test sub function
+$t->is(Calculator::sub(2,1), 1);
+$t->is(Calculator::sub(1,2), -1);
+$t->is(Calculator::sub(-12.3, 1.3), -13.6);
+
+// Test mul function
+$t->is(Calculator::mul(1,1), 1);
+$t->is(Calculator::mul(2,2), 4);
+$t->is(Calculator::mul(0.5,2), 1);
+
+// Test div function
+$t->is(Calculator::div(13, 13), 1);
+$t->is(Calculator::div(24, 2), 12);
+$t->is(Calculator::div(13, 2), 6.5);
+$t->is(Calculator::div(13, 0), null);
+
